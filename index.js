c1823de0a40426c7b4474be9dddae27b3c489d85
Jack <bartnikjack@gmail.com>
2020-08-15 22:17:50 +0100

    added github webhook integration

--- index.js
+++ index.js
@@ -187,3 +187,64 @@ function logInChannel(message) {
       .send(message);
   }
 }
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const secret = config.secret;
+
+// GitHub: X-Hub-Signature
+// Gogs:   X-Gogs-Signature
+const sigHeaderName = "X-Hub-Signature";
+
+const app = express();
+app.use(bodyParser.json());
+
+function verifyPostData(req, res, next) {
+  const payload = JSON.stringify(req.body);
+  if (!payload) {
+    return next("Request body empty");
+  }
+
+  const sig = req.get(sigHeaderName) || "";
+  const hmac = crypto.createHmac("sha1", secret);
+  const digest = Buffer.from(
+    "sha1=" + hmac.update(payload).digest("hex"),
+    "utf8"
+  );
+  const checksum = Buffer.from(sig, "utf8");
+  if (
+    checksum.length !== digest.length ||
+    !crypto.timingSafeEqual(digest, checksum)
+  ) {
+    return next(
+      `Request body digest (${digest}) did not match ${sigHeaderName} (${checksum})`
+    );
+  }
+  return next();
+}
+
+app.post("/release", verifyPostData, function (req, res) {
+  const releasedata = req.body;
+  console.log(releasedata);
+  if (releasedata.action == "released") {
+    let message = "";
+    message += `<@&${config.roles.updatePing}>\n`;
+    message += "**monkey-type " + releasedata.release.tag_name + "**";
+    message += "```\n";
+    message += releasedata.release.body + "\n";
+    message += "```";
+
+    guild.channels.cache
+      .find((ch) => ch.id === config.channels.updates)
+      .send(message);
+  }
+  res.status(200).send("Request body was signed");
+});
+
+app.use((err, req, res, next) => {
+  if (err) console.error(err);
+  res.status(403).send("Request body was not signed or verification failed");
+});
+
+app.listen(3000);
