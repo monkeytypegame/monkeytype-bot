6b17f53a0b1a6608d6aafbfd7af9d5e37dcff2be
Jack <bartnikjack@gmail.com>
2020-09-13 00:35:52 +0100

    minor changes

--- cmds/pb.js
+++ cmds/pb.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, db, guild) => {
   console.log(`Running command ${this.cmd.name}`);
@@ -36,82 +36,105 @@ module.exports.run = async (bot, message, args, db, guild) => {
 
   //embeds that display records
 
-message.channel.send(`**__Timed Personal Bests for ${message.author.username}__**`);
+  message.channel.send(
+    `**Timed Personal Bests for ${message.author.username}**`
+  );
 
   const scoreTimeEmbed = new Discord.MessageEmbed()
     .setColor("#e2b714")
-    .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/263/chart-increasing_1f4c8.png')
+    .setThumbnail(
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/263/chart-increasing_1f4c8.png"
+    )
     .setTimestamp()
-    .setFooter('https://monkey-type.com/')
-  verifyTimeDefined(15)
-  verifyTimeDefined(30)
-  verifyTimeDefined(60)
-  verifyTimeDefined(120)
+    .setFooter("https://monkey-type.com/");
+  verifyTimeDefined(15);
+  verifyTimeDefined(30);
+  verifyTimeDefined(60);
+  verifyTimeDefined(120);
   message.channel.send(scoreTimeEmbed);
 
-message.channel.send(`**__Word Personal Bests for ${message.author.username}__**`);
+  message.channel.send(
+    `**Word Personal Bests for ${message.author.username}**`
+  );
 
   const scoreWordsEmbed = new Discord.MessageEmbed()
     .setColor("#e2b714")
-    .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/clipboard_1f4cb.png')
+    .setThumbnail(
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/clipboard_1f4cb.png"
+    )
     .setTimestamp()
-    .setFooter('https://monkey-type.com/')
-  verifyWordDefined(10)
-  verifyWordDefined(25)
-  verifyWordDefined(50)
-  verifyWordDefined(100)
+    .setFooter("https://monkey-type.com/");
+  verifyWordDefined(10);
+  verifyWordDefined(25);
+  verifyWordDefined(50);
+  verifyWordDefined(100);
   message.channel.send(scoreWordsEmbed);
+
+  return {
+    status: true,
+    message: '',
+  };
 
   //functions for adding fields and finding values (some of which are a nightmare)
 
-  function findWordRaw(val) { 
-    let timeVal = val
-    let rawToFind = maxesWords[val]
-    let {raw} = pbObj.words[timeVal].find(({wpm})=>wpm===rawToFind);
-    return raw
-  };
+  function findWordRaw(val) {
+    let timeVal = val;
+    let rawToFind = maxesWords[val];
+    let { raw } = pbObj.words[timeVal].find(({ wpm }) => wpm === rawToFind);
+    return raw;
+  }
 
-  function findTimeRaw(val) { 
-    let timeVal = val
-    let rawToFind = maxesTime[val]
-    let {raw} = pbObj.time[timeVal].find(({wpm})=>wpm===rawToFind);
-    return raw
-  };
+  function findTimeRaw(val) {
+    let timeVal = val;
+    let rawToFind = maxesTime[val];
+    let { raw } = pbObj.time[timeVal].find(({ wpm }) => wpm === rawToFind);
+    return raw;
+  }
 
-  function findWordAcc(val) { 
-    let timeVal = val
-    let accToFind = maxesWords[val]
-    let {acc} = pbObj.words[timeVal].find(({wpm})=>wpm===accToFind);
-    return acc
-  };
+  function findWordAcc(val) {
+    let timeVal = val;
+    let accToFind = maxesWords[val];
+    let { acc } = pbObj.words[timeVal].find(({ wpm }) => wpm === accToFind);
+    return acc;
+  }
 
-  function findTimeAcc(val) { 
-    let timeVal = val
-    let accToFind = maxesTime[val]
-    let {acc} = pbObj.time[timeVal].find(({wpm})=>wpm===accToFind);
-    return acc
-  };
+  function findTimeAcc(val) {
+    let timeVal = val;
+    let accToFind = maxesTime[val];
+    let { acc } = pbObj.time[timeVal].find(({ wpm }) => wpm === accToFind);
+    return acc;
+  }
 
   function verifyTimeDefined(element) {
     if (maxesTime[element]) {
-      scoreTimeEmbed.addField(`**${element}s**`, `${maxesTime[element]} wpm (${findTimeRaw(element) === undefined ?'-':findTimeRaw(element)} raw) ${findTimeAcc(element)}%`);
+      scoreTimeEmbed.addField(
+        `**${element} sec**`,
+        `${maxesTime[element]} wpm (${
+          findTimeRaw(element) === undefined ? "-" : findTimeRaw(element) + ' raw'
+        }) ${findTimeAcc(element)}%`
+      );
       // scoreTimeEmbed.addField(`Raw:`, `${findTimeRaw(element) === undefined ?'-':findTimeRaw(element)} wpm`, true);
       // scoreTimeEmbed.addField(`Accuracy:`, `${findTimeAcc(element)}%`, true);
       //scoreTimeEmbed.addField(`\u200b`, `\u200b`);
-    };
-  };
+    }
+  }
 
   function verifyWordDefined(element) {
     if (maxesWords[element]) {
-      scoreWordsEmbed.addField(`**${element}s**`, `${maxesWords[element]} wpm (${findWordRaw(element) === undefined ?'-':findWordRaw(element)} raw) ${findWordAcc(element)}%`);
+      scoreWordsEmbed.addField(
+        `**${element} words**`,
+        `${maxesWords[element]} wpm (${
+          findWordRaw(element) === undefined ? "-" : findWordRaw(element) + ' raw'
+        }) ${findWordAcc(element)}%`
+      );
       // scoreWordsEmbed.addField(`Raw:`, `${findWordRaw(element) === undefined ?'-':findWordRaw(element)} wpm`, true);
       // scoreWordsEmbed.addField(`Accuracy:`, `${findWordAcc(element)}%`, true);
       //scoreWordsEmbed.addField(`\u200b`, `\u200b`);
-    };
-  };
+    }
+  }
 };
-  
-  module.exports.cmd = {
-    name: "pb",
-    needMod: false,
-  };
+
+module.exports.cmd = {
+  name: "pb",
+  needMod: false,
+};
