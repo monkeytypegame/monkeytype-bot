492d201401539d618e34eade3e817063dddb7ed7
Proto <64602574+3Proto@users.noreply.github.com>
2020-09-12 20:37:10 +0100

    updated pb.js
    made code faster and less clunky, made embed prettier - should not throw any errors now, all code is under 1 embed

--- cmds/pb.js
+++ cmds/pb.js
@@ -34,45 +34,44 @@ module.exports.run = async (bot, message, args, db, guild) => {
     ])
   );
 
-  //embeds for time and word high scores, complete with undefined validation
+  //embeds that display records
 
-  const timeHighScoreEmbed = new Discord.MessageEmbed()
+  const scoreEmbed = new Discord.MessageEmbed()
     .setColor("#e2b714")
-    .setTitle("Time Personal Bests for " + message.author.username);
-  if (maxesTime[15]) {
-    timeHighScoreEmbed.addField("15s Highscore:", `${maxesTime[15]} wpm`);
-  }
-  if (maxesTime[30]) {
-    timeHighScoreEmbed.addField("30s Highscore:", `${maxesTime[30]} wpm`);
-  }
-  if (maxesTime[60]) {
-    timeHighScoreEmbed.addField("60s Highscore:", `${maxesTime[60]} wpm`);
-  }
-  if (maxesTime[120]) {
-    timeHighScoreEmbed.addField("120s Highscore:", `${maxesTime[120]} wpm`);
-  }
-  message.channel.send(timeHighScoreEmbed);
-
-  const wordHighScoreEmbed = new Discord.MessageEmbed()
-    .setColor("#e2b714")
-    .setTitle("Word Personal Bests for " + message.author.username);
-  if (maxesWords[10]) {
-    wordHighScoreEmbed.addField("10 Word Highscore:", `${maxesWords[10]} wpm`);
-  }
-  if (maxesWords[25]) {
-    wordHighScoreEmbed.addField("25 Word Highscore:", `${maxesWords[25]} wpm`);
-  }
-  if (maxesWords[50]) {
-    wordHighScoreEmbed.addField("50 Word Highscore:", `${maxesWords[50]} wpm`);
-  }
-  if (maxesWords[100]) {
-    wordHighScoreEmbed.addField("100 Word Highscore:", `${maxesWords[100]} wpm`);
-  }
-  message.channel.send(wordHighScoreEmbed);
+    .setThumbnail(user.displayAvatarURL({ format: 'png', dynamic: true, size: 256 }))
+    .setTimestamp()
+    .setFooter('https://monkey-type.com')
+    .setTitle(`Personal Bests for ${message.author.username}`)
+    .addField("**__Time Personal Bests:__**")
+  verifyTimeDefined(15)
+  verifyTimeDefined(30)
+  verifyTimeDefined(60)
+  verifyTimeDefined(120)
+  .addField("**__Word Personal Bests:__**")
+  verifyWordDefined(10)
+  verifyWordDefined(25)
+  verifyWordDefined(50)
+  verifyWordDefined(100)
+  message.channel.send(scoreEmbed);
   return {
     status: true,
     message: ``,
   };
+
+  //functions for adding fields
+
+  function verifyTimeDefined(element) {
+    if (maxesTime[element]) {
+      scoreEmbed.addField(`${element}s Highscore:`, `${maxesTime[element]} wpm`);
+    }
+  }
+
+  function verifyWordDefined(element) {
+    if (maxesWords[element]) {
+      scoreEmbed.addField(`${element}s Highscore:`, `${maxesWords[element]} wpm`);
+    }
+  }
+
 };
 
 module.exports.cmd = {
