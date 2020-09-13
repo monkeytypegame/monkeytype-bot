3b55b6595c321505da543362cab4bc636d266630
Jack <bartnikjack@gmail.com>
2020-09-13 01:55:30 +0100

    missing %

--- cmds/announceDailyLbResult.js
+++ cmds/announceDailyLbResult.js
@@ -51,7 +51,7 @@ module.exports.run = async (bot, message, args, db, guild) => {
           embed.addFields(
             { name: 'wpm', value: winner.wpm, inline: true },
             { name: 'raw', value: winner.raw, inline: true },
-            { name: 'accuracy', value: winner.acc, inline: true },
+            { name: 'accuracy', value: winner.acc+'%', inline: true },
           )
         } else {
           //paired, tag the user
