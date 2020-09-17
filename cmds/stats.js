d5a6c86d55ff25e7f6fc55c60bd534fad0af10a1
Jack <bartnikjack@gmail.com>
2020-09-17 20:58:13 +0100

    removed commas

--- cmds/stats.js
+++ cmds/stats.js
@@ -36,8 +36,8 @@ module.exports.run = async (bot, message, args, db, guild) => {
       var m = Math.floor(d % 3600 / 60);
       var s = Math.floor(d % 3600 % 60);
 
-      var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-      var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+      var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+      var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
       var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
       return hDisplay + mDisplay + sDisplay;
     }
