82d06d425a9a421361d4648e203cd568dbe25d8e
Jack <bartnikjack@gmail.com>
2020-09-18 16:04:14 +0100

    trying to use spawn

--- cmds/restart.js
+++ cmds/restart.js
@@ -1,30 +1,22 @@
 module.exports.run = async (bot, message, args, db, guild) => {
   console.log(`Running command ${this.cmd.name}`);
-  const { exec } = require("child_process");
 
   try {
+
+
+
+
+
     setTimeout(() => {
-      exec("~/monkey-bot/restart.sh", (error, stdout, stderr) => {
-        if (error) {
-          console.log(`error: ${error.message}`);
-          return {
-            status: false,
-            message: "Could not restart: " + error.message,
-          };
-        }
-        if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return {
-            status: false,
-            message: "Could not restart: " + error.message,
-          };
-        }
-        console.log(`stdout: ${stdout}`);
-      });
-    },3000);
+
+      require("child_process").spawn("~/monkey-bot/restart.sh", [], {
+        detached: true
+      })
+
+    },1000);
     return {
       status: true,
-      message: "Restarting in 3...2...1.."
+      message: "Restarting..."
     }
   } catch (e) {
     return {
