7967161717b984775357442975541ee4728c818d
Jack <bartnikjack@gmail.com>
2020-09-23 18:23:35 +0100

    added refresh

---
+++ cmds/refresh.js
@@ -0,0 +1,18 @@
+module.exports.run = async (bot, message, args, db, guild) => {
+  console.log(`Running command ${this.cmd.name}`);
+
+  message.channel.send(
+    `Make sure you clear your cache! This is should be the first thing you try if something doesn't seem right. All it does it makes sure you get the latest code from the server.\n\nYou can do so by pressing \`CTRL/CMD + SHIFT + R\`.
+
+    `
+  );
+  return {
+    status: true,
+    message: ''
+  }
+};
+
+module.exports.cmd = {
+  name: "refresh",
+  needMod: true,
+};
