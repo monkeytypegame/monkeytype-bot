240de83da141dca605182defc8c4e523fa7a0c64
Jack <bartnikjack@gmail.com>
2020-10-10 15:43:31 +0100

    moved verification to oauth2

--- cmds/verify.js
+++ cmds/verify.js
@@ -7,67 +7,74 @@ module.exports.run = async (bot, message, args, db, guild) => {
       message: "",
     };
   }
-  if (args.length === 0) {
-    return {
-      status: false,
-      message: "Error: Please provide your pairing code",
-    };
-  }
+  // if (args.length === 0) {
+  //   return {
+  //     status: false,
+  //     message: "Error: Please provide your pairing code",
+  //   };
+  // }
 
+  //args[0] did
+  //args[1] uid
 
   let memberRole = guild.roles.cache.find(
     (role) => role.id === config.roles.memberRole
   );
-  let pairingCode = String(args[0]);
+  // let pairingCode = String(args[0]);
 
-  let usersQuery = await db
-    .collection("users")
-    .where("discordPairingCode", "==", pairingCode)
-    .get();
-  let userDocs = usersQuery.docs;
+  // let usersQuery = await db
+  //   .collection("users")
+  //   .where("discordPairingCode", "==", pairingCode)
+  //   .get();
+  // let userDocs = usersQuery.docs;
 
-  if (userDocs.length === 0) {
-    logInChannel(
-      `Could not find pairing code for <@${message.author.id}> - pairing code ${pairingCode}`
-    );
-    return {
-      status: false,
-      message: "Could not find that pairing code",
-    };
-  } else if (userDocs.length > 1) {
-    logInChannel(
-      `Multiple users with the same pairing code!!! <@Miodec> ${pairingCode} <@${message.author.id}>`
-    );
-    return {
-      status: false,
-      message: "Something went wrong! Please contact Miodec.",
-    };
-  } else if (userDocs.length === 1) {
-    let userData = userDocs[0].data();
+  // if (userDocs.length === 0) {
+  //   logInChannel(
+  //     `Could not find pairing code for <@${message.author.id}> - pairing code ${pairingCode}`
+  //   );
+  //   return {
+  //     status: false,
+  //     message: "Could not find that pairing code",
+  //   };
+  // } else if (userDocs.length > 1) {
+  //   logInChannel(
+  //     `Multiple users with the same pairing code!!! <@Miodec> ${pairingCode} <@${message.author.id}>`
+  //   );
+  //   return {
+  //     status: false,
+  //     message: "Something went wrong! Please contact Miodec.",
+  //   };
+  // } else if (userDocs.length === 1) {
+    // let userData = userDocs[0].data();
 
-    if (userData.discordId != undefined) {
-      return {
-        status: false,
-        message: "Account is already paired.",
-      };
-    }
+    // if (userData.discordId != undefined) {
+    //   return {
+    //     status: false,
+    //     message: "Account is already paired.",
+    //   };
+    // }
 
     //start pairing
-    return db
-      .collection("users")
-      .doc(userDocs[0].id)
-      .update({
-        discordId: message.author.id,
-        discordPairingCode: null
-      })
-      .then((ret) => {
+    // return db
+    //   .collection("users")
+    //   .doc(userDocs[0].id)
+    //   .update({
+    //     discordId: message.author.id,
+    //     discordPairingCode: null
+    //   })
+    //   .then((ret) => {
         return guild.members.cache
-          .find((member) => member.user.id == message.author.id)
+          .find((member) => member.user.id == args[0])
           .roles.add(memberRole)
-          .then((d) => {
+          .then(async (d) => {
             logInChannel(
-              `<@${message.author.id}> just verified their account.`
+              `<@${args[0]}> just verified their account.`
             );
+            guild.channels.cache
+            .find((ch) => ch.id === config.channels.botCommands)
+            .send(`<@${args[0]}>, your account is verified. If you have a 60s personal best, you will get a role soon.`);
+            let userData = await db.collection('users').doc(args[1]).get();
+            userData = userData.data();
             let pbs;
             try {
               pbs = userData.personalBests.time[60];
@@ -75,14 +82,14 @@ module.exports.run = async (bot, message, args, db, guild) => {
               return {
                 status: true,
                 message:
-                  "Verified, but I couldn't find any previous time 60 personal bests! Refresh the website to make sure that your accounts are paired. You should see a message 'Your accounts are paired' in the settings page.",
+                  `Verified <@${args[0]}>, but no time 60 pb found.`,
               };
             }
             if (pbs === undefined) {
               return {
                 status: true,
                 message:
-                  "Verified, but I couldn't find any previous time 60 personal bests! Refresh the website to make sure that your accounts are paired. You should see a message 'Your accounts are paired' in the settings page.",
+                `Verified <@${args[0]}>, but no time 60 pb found.`,
               };
             }
             try {
@@ -95,51 +102,50 @@ module.exports.run = async (bot, message, args, db, guild) => {
                   .collection("bot-commands")
                   .add({
                     command: "updateRole",
-                    arguments: [message.author.id, bestwpm],
+                    arguments: [args[0], bestwpm],
                     executed: false,
                     requestTimestamp: Date.now(),
                   })
                   .then((f) => {
                     return {
                       status: true,
-                      message:
-                        "Verified, and your WPM role should be updated soon (let us know if its not)! Refresh the website to make sure that your accounts are paired. You should see a message 'Your accounts are paired' in the settings page.",
+                      message: `Verified <@${args[0]} and updated role`,
                     };
                   })
                   .catch((e) => {
                     return {
                       status: true,
-                      message: `Verified, but something went wrong when trying to assign your new WPM role (${e.message})! Refresh the website to make sure that your accounts are paired. You should see a message 'Your accounts are paired' in the settings page.`,
+                      message: `Verified <@${args[0]}>. Error while finding t60 pb ${e.message}`,
                     };
                   });
               }
             } catch (e) {
               return {
                 status: true,
-                message: `Verified, but something went wrong when trying to check your previous time 60 personal bests (${e.message})! Refresh the website to make sure that your accounts are paired. You should see a message 'Your accounts are paired' in the settings page.`,
+                message: `Verified <@${args[0]}>. Error while finding t60 pb ${e.message}`,
               };
             }
           })
           .catch((e) => {
             logInChannel(
-              `Could not update role for <@${message.author.id}> - ${e}`
+              `Could not update role for <@${args[0]}> - ${e}`
             );
             return {
               status: false,
-              message: "Could not update your role: " + e,
+              message: `Could not update role for <@${args[0]}> - ${e}`
             };
           });
-      })
-      .catch((e) => {
-        logInChannel(
-          `Could not pair account for <@${message.author.id}> - ${e}`
-        );
-        return {
-          status: false,
-          message: "Could not pair to your account: " + e,
-        };
-      });
-  }
+      // })
+      // .catch((e) => {
+      //   logInChannel(
+      //     `Could not pair account for <@${message.author.id}> - ${e}`
+      //   );
+      //   return {
+      //     status: false,
+      //     message: "Could not pair to your account: " + e,
+      //   };
+      // });
+  // }
 
   function logInChannel(message) {
     if (
@@ -155,5 +161,5 @@ module.exports.run = async (bot, message, args, db, guild) => {
 
 module.exports.cmd = {
   name: "verify",
-  type: "dm",
+  type: "db",
 };
