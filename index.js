fb9db08fbaffb58e60b01a69f9a6551476192e4f
Jack <bartnikjack@gmail.com>
2020-12-06 01:25:01 +0000

    added deleted message loggin

--- index.js
+++ index.js
@@ -179,6 +179,37 @@ bot.on("message", (msg) => {
   });
 });
 
+bot.on('messageDelete', async message => {
+	// ignore direct messages
+	if (!message.guild) return;
+	const fetchedLogs = await message.guild.fetchAuditLogs({
+		limit: 1,
+		type: 'MESSAGE_DELETE',
+	});
+	// Since we only have 1 audit log entry in this collection, we can simply grab the first one
+	const deletionLog = fetchedLogs.entries.first();
+
+	// Let's perform a coherence check here and make sure we got *something*
+  if (!deletionLog) {
+    logInChannel(`:wastebasket: <@${message.author.id}>'s message was deleted: ${message.content}`);
+    return;
+  }
+	// We now grab the user object of the person who deleted the message
+	// Let us also grab the target of this action to double check things
+	const { executor, target } = deletionLog;
+
+  console.log(deletionLog);
+	// And now we can update our output with a bit more information
+	// We will also run a check to make sure the log we got was for the same author's message
+	if (target.id === message.author.id) {
+    logInChannel(`:wastebasket: <@${message.author.id}>'s message was deleted by <@${executor.id}>: ${message.content}`);
+    return;
+  } else {
+    logInChannel(`:wastebasket: <@${message.author.id}> deleted their own message: ${message.content}`);
+    return;
+	}
+});
+
 // Bot login
 bot.login(config.token);
 
