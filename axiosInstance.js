01f6d5020f019d228e2d7c9b20cae010570f1ced
lukew3 <lukew25073@gmail.com>
2021-06-05 16:03:16 -0400

    added bot authorization with customToken

--- axiosInstance.js
+++ axiosInstance.js
@@ -1,7 +1,55 @@
 import axios from "axios";
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const axiosInstance = axios.create({
   baseURL: "https://api.monkeytype.com",
 });
+
+const uid = "monkeytype-bot";
+const additionalClaims = { isDiscordBot: true };
+
+let customToken;
+customToken = admin.auth().createCustomToken(uid, additionalClaims);
+
+// Request interceptor for API calls
+axiosInstance.interceptors.request.use(
+  async config => {
+    if (customToken) {
+      config.headers = {
+        'Authorization': `Bearer ${customToken}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    } else {
+      config.headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+    return config;
+  },
+  error => {
+    Promise.reject(error)
+});
+
+// Response interceptor for API calls
+axiosInstance.interceptors.response.use((response) => {
+  return response
+}, async function (error) {
+  const originalRequest = error.config;
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    //console.log("Refreshing access token");
+    await customToken = admin.auth().createCustomToken(uid, additionalClaims);
+    return axiosApiInstance(originalRequest);
+  }
+  return Promise.reject(error);
+});
+
 
 export default axiosInstance;
