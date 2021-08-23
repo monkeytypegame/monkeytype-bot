2126c97802d9da5846fca755cdd46903406ae371
Corey <coreybergeron1@gmail.com>
2021-08-23 09:43:39 -0400

    challenge role auto assignment

---
+++ cmds/awardChallenge.js
@@ -0,0 +1,49 @@
+module.exports.run = async (bot, message, args, guild) => {
+  console.log(`Running command ${this.cmd.name} ${JSON.stringify(args)}`);
+
+  if (args.length !== 2) {
+    return {
+      status: false,
+      message: ":x: Must provide two arguments",
+    };
+  }
+
+  const config = require("../config.json");
+  let discordId = args[0];
+  let challengeName = args[1];
+
+  try {
+    let challengeRole = guild.roles.cache.find(
+      (cacheRole) => role.id === config.challenges[challengeName]
+    );
+  
+    let member = await guild.members.cache.find(
+      (member) => member.user.id == discordId
+    );
+
+    return member.roles
+      .add(challengeRole)
+      .then((ret) => {
+        return {
+          status: true,
+          message: `:white_check_mark: Assigned role ${challengeRole.name} to user <@${member.user.id}>`,
+        };
+      })
+      .catch((e) => {
+        return {
+          status: false,
+          message: e,
+        };
+      });
+  } catch (e) {
+    return {
+      status: false,
+      message: ":x: Error: Something went wrong - " + e,
+    };
+  }
+}
+
+module.exports.cmd = {
+  name: "awardChallenge",
+  type: "db",
+};
