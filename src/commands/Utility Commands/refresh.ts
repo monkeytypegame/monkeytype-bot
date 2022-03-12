fe145c5532c29ca4069f19baaca8519af82bc27d
Ferotiq <evanrayweigel@outlook.com>
2022-03-12 13:55:04 -0600

    Update refresh.ts

--- src/commands/Utility Commands/refresh.ts
+++ src/commands/Utility Commands/refresh.ts
@@ -7,7 +7,7 @@ export default {
   roles: [RolesEnum.MODERATOR, RolesEnum.ADMINISTRATOR],
   run: async (interaction) => {
     interaction.reply(
-      `Make sure you clear your cache! This is should be the first thing you try if something doesn't seem right or after Miodec fixed a bug. All it does it makes sure you get the latest code from the server.\n\nYou can do so by pressing \`CTRL/CMD + SHIFT + R\`.
+      `Make sure you clear your cache! This should be the first thing you try if something doesn't seem right or after Miodec fixed a bug. All it does it makes sure you get the latest code from the server.\n\nYou can do so by pressing \`CTRL/CMD + SHIFT + R\`.
 
       `
     );
