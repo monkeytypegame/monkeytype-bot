14622ac9ff3d4300aab2757f78b90523e91d39a1
Evan <evanrayweigel@outlook.com>
2022-04-16 15:01:51 -0500

    Add linkDiscord.ts

---
+++ src/tasks/linkDiscord.ts
@@ -0,0 +1,48 @@
+import { TaskFile } from "../interfaces/Task";
+
+export default {
+  name: "linkDiscord",
+  run: async (client, guild, discordUserID: string, userID: string) => {
+    if (discordUserID === undefined || userID === undefined) {
+      return {
+        status: false,
+        message: "Invalid parameters"
+      };
+    }
+
+    const member = guild.members.cache.get(discordUserID);
+
+    if (member === undefined) {
+      return {
+        status: false,
+        message: "Could not find user"
+      };
+    }
+
+    const memberRole = guild.roles.cache.get(
+      client.clientOptions.roles.memberRole
+    );
+
+    if (memberRole === undefined) {
+      return {
+        status: false,
+        message: "Could not find member role"
+      };
+    }
+
+    await member.roles.add(memberRole);
+
+    const botCommandsChannel = await client.getChannel("botCommands");
+
+    if (botCommandsChannel !== undefined) {
+      botCommandsChannel.send(
+        `:white_check_mark: ${member}, your account is linked.`
+      );
+    }
+
+    return {
+      status: true,
+      message: "Successfully linked discord user"
+    };
+  }
+} as TaskFile;
