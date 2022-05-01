a1df361e645259b044377d7a0664578a8fe18633
Miodec <bartnikjack@gmail.com>
2022-05-02 01:00:37 +0200

    added command to check stats

---
+++ src/commands/Dev Commands/challenge-requests-stats.ts
@@ -0,0 +1,79 @@
+/** @format */
+
+import type { Command } from "../../interfaces/Command";
+import { getStats } from "../../dal/challengeRequestStats";
+import { MessageEmbedOptions } from "discord.js";
+import differenceInDays from "date-fns/differenceInDays";
+
+export default {
+  name: "challenge-requests-stats",
+  description: "Check which mods are slacking",
+  category: "Dev",
+  options: [],
+  needsPermissions: true,
+  run: async (interaction, client) => {
+    const stats = await getStats();
+
+    const embedOptions = stats.map((stat) => {
+      const embedOptions: MessageEmbedOptions = {};
+      const member = interaction.guild?.members.cache.get(stat.userID);
+      embedOptions.title = `Challenge Requests Stats for ${member?.displayName}`;
+      embedOptions.thumbnail = {
+        url: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/clipboard_1f4cb.png"
+      };
+      embedOptions.color = 0xe2b714;
+
+      embedOptions.fields = [
+        {
+          name: "Accepted",
+          value: stat.accepted?.toString() ?? "-",
+          inline: true
+        },
+        {
+          name: "Last Accepted",
+          value: stat.lastAccepted
+            ? differenceInDays(new Date(), stat.lastAccepted) + " days ago"
+            : "-",
+          inline: true
+        },
+        {
+          name: "‎",
+          value: "‎",
+          inline: true
+        },
+        {
+          name: "Denied",
+          value: stat.denied?.toString() ?? "-",
+          inline: true
+        },
+        {
+          name: "Last Denied",
+          value: stat.lastDenied
+            ? differenceInDays(new Date(), stat.lastDenied) + " days ago"
+            : "-",
+          inline: true
+        },
+        {
+          name: "‎",
+          value: "‎",
+          inline: true
+        },
+        {
+          name: "Total",
+          value: ((stat.accepted ?? 0) + (stat.denied ?? 0)).toString(),
+          inline: true
+        }
+      ];
+      return embedOptions;
+    });
+
+    const embeds = embedOptions.map((embedOptions) => {
+      return client.embed(embedOptions);
+    });
+
+    interaction.reply({
+      embeds,
+      ephemeral: true
+    });
+  }
+} as Command;
