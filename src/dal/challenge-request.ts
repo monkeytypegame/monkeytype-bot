dcaf2272f4fcb17de4c6b4eac7050746eabddb69
Evan <evanrayweigel@outlook.com>
2022-05-06 18:31:31 -0500

    remove async as it's not needed

--- src/dal/challenge-request.ts
+++ src/dal/challenge-request.ts
@@ -3,25 +3,23 @@
 import type { MonkeyTypes } from "../types/types";
 import { mongoDB } from "../functions/mongodb";
 
-export async function getRequestCount(): Promise<number> {
-  return await mongoDB().collection("bot-challenge-requests").countDocuments();
+export function getRequestCount(): Promise<number> {
+  return mongoDB().collection("bot-challenge-requests").countDocuments();
 }
 
-export async function getRequests(): Promise<MonkeyTypes.ChallengeRequest[]> {
-  return await mongoDB().collection("bot-challenge-requests").find().toArray();
+export function getRequests(): Promise<MonkeyTypes.ChallengeRequest[]> {
+  return mongoDB().collection("bot-challenge-requests").find().toArray();
 }
 
-export async function addRequest(
-  data: MonkeyTypes.ChallengeRequest
-): Promise<void> {
-  await mongoDB().collection("bot-challenge-requests").insertOne(data);
+export function addRequest(data: MonkeyTypes.ChallengeRequest): void {
+  mongoDB().collection("bot-challenge-requests").insertOne(data);
 }
 
-export async function getRequest(
+export function getRequest(
   userID: string,
   messageID: string
 ): Promise<MonkeyTypes.ChallengeRequest | undefined> {
-  return await mongoDB()
+  return mongoDB()
     .collection<MonkeyTypes.ChallengeRequest>("bot-challenge-requests")
     .findOne({
       userID,
@@ -29,11 +27,8 @@ export async function getRequest(
     });
 }
 
-export async function deleteRequest(
-  userID: string,
-  messageID: string
-): Promise<void> {
-  await mongoDB().collection("bot-challenge-requests").deleteOne({
+export function deleteRequest(userID: string, messageID: string): void {
+  mongoDB().collection("bot-challenge-requests").deleteOne({
     userID,
     messageID
   });
