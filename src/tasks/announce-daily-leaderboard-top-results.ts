74ecfd7aa96c7daef86810fa74a277eaac77a51f
Evan <evanrayweigel@outlook.com>
2022-05-29 09:57:34 -0500

    refactor

--- src/tasks/announce-daily-leaderboard-top-results.ts
+++ src/tasks/announce-daily-leaderboard-top-results.ts
@@ -1,6 +1,5 @@
 import { MonkeyTypes } from "../types/types";
 import { Client } from "../structures/client";
-import { EmbedFieldData } from "discord.js";
 import { getPositionString } from "../functions/get-position-string";
 
 export default {
@@ -23,33 +22,21 @@ export default {
       };
     }
 
-    // console.log(
-    //   client.user.username,
-    //   guild.name,
-    //   leaderboardID,
-    //   new Date(leaderboardTimestamp),
-    //   topResults
-    // );
-
-    const fields: EmbedFieldData[] = [];
-
-    topResults.forEach((entry, i) => {
-      fields.push({
-        name: getPositionString(entry.rank ?? i + 1),
-        value: entry.name,
-        inline: true
-      });
-      fields.push({
-        name: `${entry.wpm} wpm`,
-        value: `${entry.acc}% acc`,
-        inline: true
-      });
-      fields.push({
-        name: `${entry.raw} raw`,
-        value: `${entry.consistency}% con`,
-        inline: true
-      });
-    });
+    const fields = topResults
+      .map((entry, i) => [
+        {
+          name: getPositionString(entry.rank ?? i + 1),
+          value: entry.name,
+          inline: true
+        },
+        { name: `${entry.wpm} wpm`, value: `${entry.acc}% acc`, inline: true },
+        {
+          name: `${entry.raw} raw`,
+          value: `${entry.consistency}% con`,
+          inline: true
+        }
+      ])
+      .flat();
 
     const embed = client.embed({
       title: `Daily ${leaderboardID} leaderboard result`,
