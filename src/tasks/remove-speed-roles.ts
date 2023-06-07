2b33aa652848eaf2d72fa451409c66a08901daa8
Miodec <jack@monkeytype.com>
2023-06-07 14:15:00 +0200

    added remove speed roles task

---
+++ src/tasks/remove-speed-roles.ts
@@ -0,0 +1,33 @@
+import type { MonkeyTypes } from "../types/types";
+
+export default {
+  name: "updateRole",
+  run: async (client, guild, discordUserID: string) => {
+    if (discordUserID === undefined) {
+      return {
+        status: false,
+        message: "Invalid parameters",
+        member: discordUserID
+      };
+    }
+
+    const member = await guild.members
+      .fetch(discordUserID)
+      .catch(() => undefined);
+
+    if (member === undefined) {
+      return {
+        status: false,
+        message: "Member not found",
+        member: discordUserID
+      };
+    }
+
+    await client.removeAllWPMRoles(member);
+
+    return {
+      status: true,
+      message: "Successfully removed all speed roles."
+    };
+  }
+} as MonkeyTypes.TaskFile;
