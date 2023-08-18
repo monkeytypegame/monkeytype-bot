5959405c2a80ca602c0bfb94be69c287bc683f39
Miodec <jack@monkeytype.com>
2023-08-18 12:48:27 +0200

    changed release to send a link to github releases
    (also added a text only and embed only version in case i want to switch later)

--- src/tasks/send-release-announcement.ts
+++ src/tasks/send-release-announcement.ts
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 import type { MonkeyTypes } from "../types/types";
-import { createChunks } from "../utils/chunks";
 
 export default {
   name: "sendReleaseAnnouncement",
   run: async (client, guild, releaseID) => {
-    console.log("Sent a new release, sending to #updates channel...");
+    console.log(
+      `Sent a new release (${releaseID}), sending to #updates channel...`
+    );
 
     const channel = await client.getChannel("updates");
 
@@ -40,15 +41,59 @@ export default {
       };
     }
 
-    await channel.send(`${updateRole}\n**Monkeytype ${release.name}**`);
+    // const ping = `${updateRole}\n***monkeytype ${release.name}***`;
 
-    for (const message of createChunks(
-      release.body,
-      2000 - "```\n\n```".length,
-      "\n\n"
-    )) {
-      await channel.send(`\`\`\`\n${message}\n\`\`\``);
+    // for (const message of createChunks(
+    //   release.body,
+    //   2000 - "```\n\n```".length,
+    //   "\n\n"
+    // )) {
+    //   await channel.send(`\`\`\`\n${message}\n\`\`\``);
+    // }
+
+    //wrap all links in <> to prevent embeds
+    release.body = release.body.replace(/\((https?:\/\/[^\s]+?)\)/g, "(<$1>)");
+
+    const split = release.body.split(`\r\n\r\n###`);
+
+    // const title = split.shift();
+
+    for (let i = 0; i < split.length; i++) {
+      split[i] = `###${split[i]}`;
     }
+
+    const embeds = [];
+
+    let invisibleLine = "";
+    for (let i = 0; i < 2000; i++) {
+      invisibleLine += " ";
+    }
+
+    for (const s of split) {
+      const [t, l] = s.split("\r\n\r\n");
+      const title = t?.replace("### ", "");
+      embeds.push({
+        title: title,
+        description: invisibleLine + "\n" + l,
+        color: 0xe2b714
+      });
+    }
+
+    // await channel.send(ping + "\n" + title);
+    // for (let i = 0; i < split.length; i++) {
+    //   const message = split[i] as string;
+    //   await channel.send(message);
+    // }
+
+    // await channel.send(ping);
+    // await channel.send({
+    //   content: `${updateRole}\n***monkeytype ${release.name}***\n${title}`,
+    //   embeds: embeds
+    // });
+
+    await channel.send(
+      `${updateRole}\nVersion **${release.name}** has been released! Check out the full change log here:\n${release.html_url}\nor by clicking the version number on the website.`
+    );
 
     return {
       status: true,
